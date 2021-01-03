15e6be9ae05e46620e84fee36a7d2a591a39b712
Damon Wu <damonmaozewu@gmail.com>
2021-01-03 16:11:27 +1100

    first chrome extension draft

--- popup.js
+++ popup.js
@@ -1,30 +1,69 @@
-// function createChoice(event) {
-//   let currentChoice = event.target.value;
-//   // let currentChoice = 'fake one'; // will remove
-//   let choicesArray = [];
-//   choicesArray.push(currentChoice);
-//   for(let i = 0; i < choicesArray.length; i++) {
-//     const eleLi = document.getElementById('addChoice').appendChild('li');
-//     eleLi.innerHTML = "Choice: " + choicesArray[i];
-//   }
-// }
-// alert("alert: 1");
+var choicesList = [];
 
-document.getElementById('inputChoice').addEventListener('input', getCurrentChoice);
-document.getElementById('addChoice').addEventListener('click', createNewNode);
+// DOM with Id
+
+function domWithId(id) {
+  return document.getElementById(id);
+}
+
+// actions
+
+domWithId('inputChoice').addEventListener('input', getCurrentChoice);
+domWithId('inputChoice').addEventListener('keypress', preventEnterPress);
+domWithId('addChoice').addEventListener('click', createNewNode);
+domWithId('maker').addEventListener('click', choiceMaker);
+
+// helper functions
+
+function preventEnterPress(event) {
+  if(event.keyCode === 13 || event.key === 'Enter') {
+    return event.preventDefault();
+  }
+}
 
 function getCurrentChoice(event) {
-  document.getElementById('choiceValue').textContent = event.target.value;
+  domWithId('choiceValue').textContent = event.target.value;
+
+  if (domWithId('choiceValue').textContent.length > 20) {
+    domWithId('invalidInput').textContent = 'Oops, maximum input characters are 20.';
+    domWithId('addChoice').disabled = true;
+  } else if(
+    domWithId('choiceValue').textContent.length === 0 || domWithId('choiceValue').textContent === '') {
+    domWithId('invalidInput').textContent = 'Oops, choice input cannot be empty';
+    domWithId('addChoice').disabled = true;
+  } else {
+    domWithId('invalidInput').textContent = '';
+    domWithId('addChoice').style.display = 'block';
+    domWithId('addChoice').disabled = false;
+  }
 }
 
 function createNewNode() {
-  var li=document.createElement("li");
-  document.getElementById("choiceList").appendChild(li);
-  li.innerText=document.getElementById("choiceValue").textContent;
+  // add new node
+  var li = document.createElement("li");
+  domWithId("choiceList").appendChild(li);
+  li.innerText = domWithId("choiceValue").textContent;
+  choicesList.push(domWithId("choiceValue").textContent);
+
+  // reset input & display value to empty
+  domWithId("inputChoice").value = "";
+  domWithId('choiceValue').textContent = "";
+
+  // show choice maker button
+  if (choicesList.length > 1 && domWithId("maker").classList.contains('hide')) {
+    domWithId("maker").classList.remove('hide');
+    domWithId("maker").style.display = "block";
+  }
 }
 
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   chrome.tabs.executeScript({
-//     code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
-//   });
-// });
+function choiceMaker() {
+  domWithId('spinner').style.display = "block";
+  domWithId('choiceForm').style.display = "none";
+
+  setTimeout(() => {
+    domWithId('spinner').style.display = "none";
+    domWithId('choiceForm').style.display = "block";
+    domWithId("fateBox").classList.remove('hide');
+    domWithId('fateValue').textContent = choicesList[Math.floor(Math.random() * choicesList.length)];
+  }, 3000);
+}
